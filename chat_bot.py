09b2cd2b3716c0ec41a362683ee2d5133fb4ced5
obsoletewoosh <ryan_leschensky@brown.edu>
2023-07-07 13:08:36 -0400

    Added chat_bot.py
    Manage complexity :)

---
+++ chat_bot.py
@@ -0,0 +1,34 @@
+import spacy
+
+# Importing Spacy Language Models
+import en_core_web_sm
+import en_core_web_trf
+import en_core_web_md
+
+
+class ChatBot:
+    def __init__(self, language_model, min_similarity):
+        self.language_model = language_model
+        self.nlp = spacy.load(self.language_model)
+        self.min_similarity = min_similarity
+
+    def __str__(self):
+        return str(self.language_model)
+
+    def __eq__(self, other):
+        if type(other) is type(self):
+            return self.language_model == other.language_model
+        return False
+
+    def respond(self, msg):
+        guide_statement = self.nlp("You are a chatbot who wants to help the user answer their question.")
+        statement = self.nlp(msg)
+
+        print("...")
+
+        for ent in statement.ents:
+            print(ent.label_, ent.text)
+
+
+
+
--- main.py
+++ main.py
@@ -5,46 +5,46 @@ import en_core_web_sm
 import en_core_web_trf
 import en_core_web_md
 
+from chat_bot import ChatBot
+
 # Definine Global variables with None so that they can be defined in __init__ to manage complexity
 nlp = None
-efficient_mode = None
 
 
-def __init__(efficient):
+def _init(language_model):
     global nlp
-    global efficient_mode
-
-    efficient_mode = efficient
 
     # Model difference based upon efficient boolean since one is faster than the other
     # The slower model is more accurate so there is a tradeoff which needs to be specified at runtime
 
-    if efficient:
-        nlp = spacy.load('en_core_web_sm')
+    if language_model == 'en_core_web_sm':
+        nlp = spacy.load(language_model)
         nlp = en_core_web_sm.load()
-    else:
-        nlp = spacy.load('en_core_web_md')
+    elif language_model == 'en_core_web_md':
+        nlp = spacy.load(language_model)
         nlp = en_core_web_md.load()
 
 
 def to_token_list(doc):
     return [token for token in doc]
 
-def 
 
 if __name__ == '__main__':
-    __init__(efficient=True)
+    # _init(language_model='en_core_web_md')
 
     # All warnings related to 'nlp' not being callable should be ignored
     # nlp is defined during runtime
 
-    doc = nlp("Some sentance here")
-    tokens = to_token_list(doc)
-    print(tokens)
+    # doc = nlp("Some sentance here")
+    # tokens = to_token_list(doc)
+    # print(tokens)
 
-    doc1 = nlp(input("Insert sentence one!"))
-    doc2 = nlp(input("Insert sentence two!"))
+    a = ChatBot(language_model='en_core_web_md', min_similarity=0.50)
+    a.respond("I really want to go to New York... Or Rhode Island! The school named Brown is really interesting! Where is Brown and is it nice?")
 
-    similarity = doc1.similarity(doc2)
+    # doc1 = nlp(input("Insert sentence one!"))
+    # doc2 = nlp(input("Insert sentence two!"))
 
-    print(f"The similarity between sentence one and two are: {similarity * 100:.0f}%")
+    # similarity = doc1.similarity(doc2)
+
+    # print(f"The similarity between sentence one and two are: {similarity * 100:.0f}%")
