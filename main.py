855687fde0f688a07ba940c8893bfea59d2b9e92
obsoletewoosh <ryan_leschensky@brown.edu>
2023-07-07 11:11:01 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,38 @@
 import spacy
 import en_core_web_sm
+import en_core_web_trf
 
-nlp = spacy.load('en_core_web_sm')
+nlp = None
 
 
-def __init__():
-    nlp = en_core_web_sm.load()
-    doc = nlp("Hello World! See how parts of the sentence are separated into different groups!")
+def __init__(efficient):
+    global nlp
 
-    for token in doc:
-        print(f"""
-        TEXT  : {token.text} -- Raw text of the word
-        LEMMA : {token.lemma_} -- Base form of the token, with no inflectional suffixes.
-        POS_  : {token.pos_} -- Coarse-grained part-of-speech from the Universal POS tag set (String)
-        POS   : {token.pos} -- Same as POS_ but instead has an integer value
-        TAG_  : {token.tag_} -- Fine-grained part-of-speech.
-        TAG   : {token.tag} -- Same as TAG_ but instead returns an integer value
-        DEP_  : {token.dep_} -- Syntactic dependency relation
-        DEP   : {token.dep} -- Returns the integer value of DEP 
-        SHAPE : {token.shape_} -- Transform of the token’s string to show orthographic features X for alpha d for numeric
-        ALPHA : {token.is_alpha} 
-        STOP  : {token.is_stop}
-        """)
+    if efficient:
+        nlp = spacy.load('en_core_web_sm')
+        nlp = en_core_web_sm.load()
+    else:
+        nlp = spacy.load('en_core_web_trf')
+        nlp = en_core_web_trf.load()
 
-    print([(w.text, w.pos_, w.pos) for w in doc])
+
+def to_token_list(doc):
+    return [token for token in doc]
+
 
 if __name__ == '__main__':
-    __init__()
+    __init__(efficient=True)
+
+    doc = nlp("Some sentance here")
+    tokens = to_token_list(doc)
+    print(tokens)
+
+    doc1 = nlp(input("Insert sentence one!"))
+    doc2 = nlp(input("Insert sentence two!"))
+
+    similarity = doc1.similarity(doc2)
+
+    print(f"The similarity between sentence one and two are: {similarity*100:.0f}%")
+
+
+
