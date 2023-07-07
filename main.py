43d153ff900bd43949f7c3ca09705a930acf6b0b
obsoletewoosh <ryan_leschensky@brown.edu>
2023-07-07 12:22:47 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import spacy
+
+# Importing Spacy Language Models
 import en_core_web_sm
 import en_core_web_trf
+import en_core_web_md
 
+# Definine Global variables with None so that they can be defined in __init__ to manage complexity
 nlp = None
 efficient_mode = None
+
 
 def __init__(efficient):
     global nlp
@@ -11,20 +16,27 @@ def __init__(efficient):
 
     efficient_mode = efficient
 
+    # Model difference based upon efficient boolean since one is faster than the other
+    # The slower model is more accurate so there is a tradeoff which needs to be specified at runtime
+
     if efficient:
         nlp = spacy.load('en_core_web_sm')
         nlp = en_core_web_sm.load()
     else:
-        nlp = spacy.load('en_core_web_trf')
-        nlp = en_core_web_trf.load()
+        nlp = spacy.load('en_core_web_md')
+        nlp = en_core_web_md.load()
 
 
 def to_token_list(doc):
     return [token for token in doc]
 
+def 
 
 if __name__ == '__main__':
     __init__(efficient=True)
+
+    # All warnings related to 'nlp' not being callable should be ignored
+    # nlp is defined during runtime
 
     doc = nlp("Some sentance here")
     tokens = to_token_list(doc)
@@ -35,7 +47,4 @@ if __name__ == '__main__':
 
     similarity = doc1.similarity(doc2)
 
-    print(f"The similarity between sentence one and two are: {similarity*100:.0f}%")
-
-    #Ok
-
+    print(f"The similarity between sentence one and two are: {similarity * 100:.0f}%")
