4092a0042a359780d5f7a1ce975a1a29779aebff
obsoletewoosh <138884188+obsoletewoosh@users.noreply.github.com>
2023-07-07 11:19:53 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,38 @@
 import spacy
 import en_core_web_sm
+import en_core_web_trf
 
-nlp = spacy.load('en_core_web_sm')
+nlp = None
+efficient_mode = None
 
-def _init():
-    nlp = en_core_web_sm.load()
-    doc = nlp("Hello World! See how parts of the sentence are separated into different groups!")
+def __init__(efficient):
+    global nlp
+    global efficient_mode
 
-    print([(w.text, w.pos_) for w in doc])
+    efficient_mode = efficient
+
+    if efficient:
+        nlp = spacy.load('en_core_web_sm')
+        nlp = en_core_web_sm.load()
+    else:
+        nlp = spacy.load('en_core_web_trf')
+        nlp = en_core_web_trf.load()
+
+
+def to_token_list(doc):
+    return [token for token in doc]
 
 
 if __name__ == '__main__':
-    _init()
+    __init__(efficient=True)
+
+    doc = nlp("Some sentance here")
+    tokens = to_token_list(doc)
+    print(tokens)
+
+    doc1 = nlp(input("Insert sentence one!"))
+    doc2 = nlp(input("Insert sentence two!"))
+
+    similarity = doc1.similarity(doc2)
+
+    print(f"The similarity between sentence one and two are: {similarity*100:.0f}%")
