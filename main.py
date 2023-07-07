4cf064c296185815307d38fb974077b234c766bf
obsoletewoosh <138884188+obsoletewoosh@users.noreply.github.com>
2023-07-07 09:59:09 -0400

    Update main.py
    Added started code :)

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-import pandas as pd
+import spacy
+import en_core_web_sm
 
-def _init() :
-    print("2")
+nlp = spacy.load('en_core_web_sm')
+
+def _init():
+    nlp = en_core_web_sm.load()
+    doc = nlp("Hello World! See how parts of the sentence are separated into different groups!")
+
+    print([(w.text, w.pos_) for w in doc])
+
 
 if __name__ == '__main__':
     _init()
-    print("3")
